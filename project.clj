cc2ae6f6c39d31a559e8350bb601b6d339443ac0
reborg <reborg@reborg.net>
2012-07-23 05:37:43 +0100

    now with a working localhost 8888 hello world

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject iad "0.1"
   :description "IAD Conference Rest Server"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [midje "1.4.0"]]
-  :dev-dependencies [[lein-midje "1.0.10"]]
+                 [ring "1.1.1"]
+                 [compojure "1.1.1"]]
+                 
+  :dev-dependencies [[lein-midje "1.0.10"]
+                     [midje "1.4.0"]]
   :main iad.core)
--- src/iad/core.clj
+++ src/iad/core.clj
@@ -1,4 +1,17 @@
-(ns iad.core)
+(ns iad.core
+  (:use ring.adapter.jetty
+        ring.middleware.reload
+        ring.middleware.stacktrace
+        ring.util.response
+        compojure.core)
+  (:require [compojure.route :as route]))
+
+(defn index "Show all events" [req]
+  (response (str "Look ma no wheels")))
+
+(defroutes app
+    (GET "/" [] "<h1>Hello World</h1>")
+    (route/not-found "<h1>Page not found</h1>"))
 
 (defn -main [& port]
-  (println "look ma, no wheels"))
+  (run-jetty #'app {:port (or port 8888) :join? false}))

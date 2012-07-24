06f107b8e4e17c99696a6e450dd593b1202f43ac
reborg <reborg@reborg.net>
2012-07-24 06:39:20 +0100

    migrated project to leiningen 2

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject iad "0.1"
-  :description "IAD Conference Rest Server"
+(defproject iad/iad "0.1" 
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring "1.1.1"]
                  [ring-json-params "0.1.3"]
                  [clj-json "0.5.1"]
                  [compojure "1.1.1"]]
-                 
-  :dev-dependencies [[lein-midje "1.0.10"]
-                     [midje "1.4.0"]]
-  :main iad.core)
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :main iad.core
+  :min-lein-version "2.0.0"
+  :plugins [[lein-midje "1.0.10"]]
+  :description "IAD Conference Rest Server")

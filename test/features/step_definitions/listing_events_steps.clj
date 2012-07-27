2852bf82c86e4846a38a31cccc2a192095b653bd
reborg <reborg@reborg.net>
2012-07-27 10:32:08 +0100

    possibily the last change to the project.clj to adjust for plugins and dependencies

--- test/features/step_definitions/listing_events_steps.clj
+++ test/features/step_definitions/listing_events_steps.clj
@@ -1,10 +1,10 @@
 (use 'iad.core)
 
 (Given #"^the conference is still far away in the future$" []
-       (start))
+       (let [server (start)]))
 
 (When #"^I request the list of the events$" []
   (comment rest call))
 
 (Then #"^the list of events is empty$" []
-      (assert (= "same" "same")))
+      (assert (= "sae" "same")))

266f64e941846f6d3436e55af20e4207bd26067e
Uberto Barbini <uberto.gama@gmail.com>
2012-07-31 09:20:04 +0100

    added some simple json parsing tests

--- test/iad/test/core.clj
+++ test/iad/test/core.clj
@@ -2,9 +2,18 @@
   (:use [iad.core])
   (:use [midje.sweet]))
 
-(facts "there are facts"
-       (fact "some of them are true"
-             (= "" "") => truthy))
+(require '[clj-json.core :as json])
 
-1
-3
+(facts "json parsing"
+  (fact "the empty list is a valid json"
+    (json/parse-string "[]") => [])
+
+
+    (fact "the 3 elements list"
+      (json/parse-string "[1,2,3]") => [1,2,3])
+
+  (fact "single event json"
+    (json/parse-string "[{\"id\":1, \"from\":\"2012-11-26\", \"to\": \"2012-11-26\",
+\"name\":\"Italian Agile Day 2012\"}]") =>  [{"id" 1, "from" "2012-11-26", "to" "2012-11-26", "name" "Italian Agile Day 2012"}])
+
+  )
